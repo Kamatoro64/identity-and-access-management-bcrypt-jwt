a4efaf7f60be447d7e567e0e718448467d704d10
Kamatoro64 <chester91.jp@gmail.com>
2020-12-05 20:31:47 +0800

    Create a route that handles JWT. Note that the tokens in the .env file are generated by node. In a node cli, run require('crypto').randomytes(64).toString('hex')

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+
 const app = express()
+app.use(express.json()) //Middleware that enables Express to use JSON passed in the body of requests
+
 
 const posts = [
 	{
@@ -16,7 +20,22 @@ const posts = [
 	}
 ]
 
-app.get('/posts', (req, res) => {
+app.post('/posts', (req, res) => {
 	res.json(posts)
+})
+
+app.get('/login', (req, res) => {
+	// Authenticate User (Separate tutorial)
+
+	// Here we assume that the user has already been authenticated with username and password
+	// We're going to authenticate and serialise this user with JWT
+	const username = req.body.username;
+
+	const user = { name: username }
+
+	jwt.sign()
+
+
+
 })
 app.listen(3000)
