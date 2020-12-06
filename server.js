6890cddd0afe4c0e165fef40c724e24621cbf53a
Kamatoro64 <chester91.jp@gmail.com>
2020-12-06 11:48:52 +0800

    Modify login endpoint to authenticate user using bcrypt.copare and return JWT token to user upon successful authentication. User can now user token to access posts endpoint to get posts belonging to user

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const users = []
 
 // Get posts (authorisation based on username to return specific posts)
 app.get('/posts', authenticateToken, (req, res) => {
-	res.json(posts.filter(post => post.username === req.user.name))
+	res.json(posts.filter(post => post.username === req.user.username))
 })
 
 // Get users 
@@ -39,15 +39,13 @@ app.get('/users', (req, res) => {
 	res.json(users)
 })
 
-app.post('/users', async (req, res) => { // Bcrypt is an asynchronous library so asynchronous (callback) function is required
-	// The goal here is to save the user in the users array (or database), without a plaintext password!
+app.post('/users', async (req, res) => {
+
 	try {
 		// const salt = await bcrypt.genSalt()
 		// const hashedPassword = await bcrypt.hash(req.body.password, salt)
-		const hashedPassword = await bcrypt.hash(req.body.password, 10) //Generate salt and hash password in 1 step
-		// console.log(salt)
-		console.log(hashedPassword)
-		const user = { name: req.body.name, password: hashedPassword }
+		const hashedPassword = await bcrypt.hash(req.body.password, 10)
+		const user = { username: req.body.username, password: hashedPassword }
 		users.push(user)
 		console.log(users)
 		res.status(201).send()
@@ -55,22 +53,29 @@ app.post('/users', async (req, res) => { // Bcrypt is an asynchronous library so
 		res.status(500)
 	}
 
-
 })
 
 // Login 
-app.post('/login', (req, res) => {
-	// Authenticate User (Separate tutorial)
+app.post('/login', async (req, res) => { // Using bcrypt asynchronous library
+	// Authenticate User 
+	const user = users.find(user => user.username === req.body.username)
+	if (user == null) {
+		return res.status(400).send('Cannot find user')
+	}
 
-	// Here we assume that the user has already been authenticated with username and password
-	// We're going to authenticate and serialise this user with JWT
-	const username = req.body.username;
+	console.log(`Checking password`)
+	await bcrypt.compare(req.body.password, user.password).then(result => {
+		if (result) {
+			console.log(`Password verified! Login successful`)
+			const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+			res.json({ accessToken: accessToken })
+		} else {
+			res.send('Not allowed. Invalid username, password combination')
+		}
+	}).catch((err) => console.error(err))
 
-	const user = { name: username }
 
-	const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
 
-	res.json({ accessToken: accessToken })
 })
 
 
